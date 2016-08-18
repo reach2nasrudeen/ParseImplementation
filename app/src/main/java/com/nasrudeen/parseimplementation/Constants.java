3a620e2eb6e6579757f8c8318e23de57007068b7
Nasrudeen <reach2nasrudeen@gmail.com>
2016-08-18 14:56:01 +0530

    Timeago Implemented

---
+++ app/src/main/java/com/nasrudeen/parseimplementation/Constants.java
@@ -0,0 +1,7 @@
+package com.nasrudeen.parseimplementation;
+
+public class Constants {
+    public final static String PARSE_APP_ID = "";
+    public final static String PARSE_APP_CLIENT_KEY = "";
+    public final static String PARSE_APP_SERVER_URL = "";
+}
